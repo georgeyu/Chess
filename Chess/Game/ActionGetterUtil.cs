592261e7747b39e800e1ba7d849b6dfc6b9f94ce
George Yu <georgelyu@gmail.com>
2016-04-15 23:18:40 -0500

    Move getting legal captures to a separate class

---
+++ Chess/Game/ActionGetterUtil.cs
@@ -0,0 +1,94 @@
+﻿using Chess.Game.Pieces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.Game
+{
+    internal class ActionGetterUtil
+    {
+        public static T[] GetActionsIgnoringLegality<T>(
+            Square[,] board,
+            bool isWhiteTurn,
+            int files,
+            int ranks,
+            Func<Square, int, int, bool, T[]> getActionsFromSquare)
+        {
+            var actionList = new List<T>();
+            for (var i = 0; i < files; i++)
+            {
+                for (var j = 0; j < ranks; j++)
+                {
+                    var actionsFromSquare = getActionsFromSquare(board[i, j], i, j, isWhiteTurn);
+                    actionList.AddRange(actionsFromSquare);
+                }
+            }
+            var actionArray = actionList.ToArray();
+            return actionArray;
+        }
+
+        public static T[] GetActionsFromSquare<T, U>(
+            Square square,
+            int file,
+            int rank,
+            bool isWhiteTurn,
+            Func<U, T> getAbsoluteFromRelative,
+            Func<Piece, U[]> getActions)
+        {
+            var actionAbsoluteList = new List<T>();
+            var actionAbsoluteArray = actionAbsoluteList.ToArray();
+            var isEmpty = square is EmptySquare;
+            if (isEmpty)
+            {
+                return actionAbsoluteArray;
+            }
+            var piece = square as Piece;
+            if (piece == null)
+            {
+                throw new NotImplementedException("Unable to cast Square as Piece.");
+            }
+            if (piece.IsWhite != isWhiteTurn)
+            {
+                return actionAbsoluteArray;
+            }
+            U[] actionsRelative = getActions(piece);
+            var startSquare = new SquareAbsolute(file, rank);
+            foreach (var actionRelative in actionsRelative)
+            {
+                T actionAbsolute = getAbsoluteFromRelative(actionRelative);
+                actionAbsoluteList.Add(actionAbsolute);
+            }
+            actionAbsoluteArray = actionAbsoluteList.ToArray();
+            return actionAbsoluteArray;
+        }
+
+        public static bool IsSquareOnBoard(SquareAbsolute square, int files, int ranks)
+        {
+            return
+                (square.File < files) &&
+                (square.File >= 0) &&
+                (square.Rank < ranks) &&
+                (square.Rank >= 0);
+        }
+
+        public static bool AreSquaresOnBoard(SquareAbsolute[] squares, int files, int ranks)
+        {
+            var isSquareOnBoard = squares.Select(x => IsSquareOnBoard(x, files, ranks));
+            bool areSquaresOnBoard = isSquareOnBoard.Aggregate((x, y) => x && y);
+            return areSquaresOnBoard;
+        }
+
+        public static bool ArePassingSquaresEmpty(SquareAbsolute[] passingSquares, Square[,] board)
+        {
+            if (passingSquares.Length == 0)
+            {
+                return true;
+            }
+            var isPassingSquareEmpty = passingSquares.Select(x => board[x.File, x.Rank] is EmptySquare);
+            var arePassingSquaresEmpty = isPassingSquareEmpty.Aggregate((x, y) => x && y);
+            return arePassingSquaresEmpty;
+        }
+    }
+}
---
+++ Chess/Game/CaptureGetter.cs
@@ -0,0 +1,90 @@
+﻿using Chess.Game.Pieces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.Game
+{
+    internal static class CaptureGetter
+    {
+        public static CaptureAbsolute[] GetCaptures(Position position)
+        {
+            int files = position.Board.GetLength(Constants.FileIndex);
+            int ranks = position.Board.GetLength(Constants.RankIndex);
+            var capturesIgnoringLegality = ActionGetterUtil.GetActionsIgnoringLegality(
+                position.Board,
+                position.IsWhiteTurn,
+                files,
+                ranks,
+                GetCapturesFromSquare);
+            var capturesStayingOnBoard = GetCapturesStayingOnBoard(capturesIgnoringLegality, files, ranks);
+            var capturesWithEmptyPassingSquares = GetCapturesWithEmptyPassingSquares(
+                capturesStayingOnBoard,
+                position.Board);
+            var capturesWhereFinalSquareIsEnemyPiece = GetCapturesWhereCaptureSquareIsEnemyPiece(
+                capturesWithEmptyPassingSquares,
+                position.Board);
+            var captures = capturesWhereFinalSquareIsEnemyPiece.ToArray();
+            return captures;
+        }
+
+        private static CaptureAbsolute[] GetCapturesFromSquare(Square square, int file, int rank, bool isWhiteTurn)
+        {
+            var startSquare = new SquareAbsolute(file, rank);
+            var captures = ActionGetterUtil.GetActionsFromSquare(
+                square,
+                file,
+                rank,
+                isWhiteTurn,
+                x => new CaptureAbsolute(
+                    startSquare,
+                    new SquareAbsolute(file + x.CaptureSquare.FileChange, rank + x.CaptureSquare.RankChange),
+                    x.PassingSquares.Select(
+                        y => new SquareAbsolute(file + y.FileChange, rank + y.RankChange)).ToArray()),
+                x => x.GenerateCaptures());
+            return captures;
+        }
+
+        private static IEnumerable<CaptureAbsolute> GetCapturesStayingOnBoard(CaptureAbsolute[] captures, int files, int ranks)
+        {
+            var capturesStayingOnBoard = captures.Where(
+                x =>
+                    ActionGetterUtil.IsSquareOnBoard(x.CaptureSquare, files, ranks) &&
+                    (
+                        (x.PassingSquares.Length == 0) ||
+                        ActionGetterUtil.AreSquaresOnBoard(x.PassingSquares, files, ranks)
+                    ));
+            return capturesStayingOnBoard;
+        }
+
+        private static IEnumerable<CaptureAbsolute> GetCapturesWithEmptyPassingSquares(
+            IEnumerable<CaptureAbsolute> captures,
+            Square[,] board)
+        {
+            var capturesWithEmptyPassingSquares = captures.Where(
+                x => ActionGetterUtil.ArePassingSquaresEmpty(x.PassingSquares, board));
+            return capturesWithEmptyPassingSquares;
+        }
+
+        private static IEnumerable<CaptureAbsolute> GetCapturesWhereCaptureSquareIsEnemyPiece(
+            IEnumerable<CaptureAbsolute> captures,
+            Square[,] board)
+        {
+            var capturesWhereCaptureSquareIsEnemyPiece = captures.Where(x => IsCaptureSquareEnemyPiece(x, board));
+            return capturesWhereCaptureSquareIsEnemyPiece;
+        }
+
+        private static bool IsCaptureSquareEnemyPiece(CaptureAbsolute capture, Square[,] board)
+        {
+            if (board[capture.CaptureSquare.File, capture.CaptureSquare.Rank] is EmptySquare)
+            {
+                return false;
+            }
+            var piece = board[capture.StartSquare.File, capture.StartSquare.Rank] as Piece;
+            var pieceToCapture = board[capture.CaptureSquare.File, capture.CaptureSquare.Rank] as Piece;
+            return piece.IsWhite != pieceToCapture.IsWhite;
+        }
+    }
+}
